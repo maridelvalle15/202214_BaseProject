fb2f7d87804a71b92b79785a7b3ab43eff1110a4
Marisela Andrea Del Valle Morillo <marisela.valle@mercadolibre.com.co>
2022-09-17 14:55:26 -0500

    implementada logica para ciudad

--- src/ciudad/ciudad.service.ts
+++ src/ciudad/ciudad.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
 import { Repository } from 'typeorm';
 import { CiudadEntity } from './ciudad.entity';
 
@@ -7,26 +8,58 @@ import { CiudadEntity } from './ciudad.entity';
 export class CiudadService {
     constructor(
         @InjectRepository(CiudadEntity)
-        private readonly ciudadRepository: Repository<CiudadEntity>
-    ){}
+        private readonly ciudadRepository: Repository<CiudadEntity>,
+        private readonly paises: string[]
+    ){
+        this.paises = ["Argentina", "Ecuador", "Paraguay"];
+    }
 
     async findAll(): Promise<CiudadEntity[]> {
-        return true;
+        return await this.ciudadRepository.find({relations: ["supermercados"]})
     }
 
-    async findOne(id: string): Promise<CiudadEntity[]> {
-        return true;
+    async findOne(id: string): Promise<CiudadEntity> {
+        const ciudad: CiudadEntity = await this.ciudadRepository.findOne({where: {id}, relations: ["supermercados"]});
+
+        if (!ciudad)
+            throw new BusinessLogicException("The ciudad with the given id was not found", BusinessError.NOT_FOUND);
+
+        return ciudad;
     }
 
-    async create(ciudad: CiudadEntity): Promise<CiudadEntity[]> {
-        return true;
+    async create(ciudad: CiudadEntity): Promise<CiudadEntity> {
+        let nombre = ciudad.nombre;
+        let valid_pais = this.paises.includes(nombre);
+
+        if (!valid_pais) {
+            throw new BusinessLogicException("The given pais for ciudad is not valid", BusinessError.PRECONDITION_FAILED);
+        }
+        return await this.ciudadRepository.save(ciudad);
     }
 
-    async update(id: string, ciudad: CiudadEntity): Promise<CiudadEntity[]> {
-        return true;
+    async update(id: string, ciudad: CiudadEntity): Promise<CiudadEntity> {
+        let nombre = ciudad.nombre;
+        let valid_pais = this.paises.includes(nombre);
+
+        if (!valid_pais) {
+            throw new BusinessLogicException("The given pais for ciudad is not valid", BusinessError.PRECONDITION_FAILED);
+        }
+        
+        const persistedCiudad: CiudadEntity = await this.ciudadRepository.findOne({where: {id}});
+
+        if (!persistedCiudad)
+            throw new BusinessLogicException("The ciudad with the given id was not found", BusinessError.NOT_FOUND);
+
+        return await this.ciudadRepository.save({...persistedCiudad, ...ciudad});
     }
 
     async delete(id: string) {
-        return true;
+        const ciudad: CiudadEntity = await this.ciudadRepository.findOne({where: {id}});
+
+        if (!ciudad)
+            throw new BusinessLogicException("The ciudad with the given id was not found", BusinessError.NOT_FOUND);
+
+        return await this.ciudadRepository.remove(ciudad);
+
     }
 }
