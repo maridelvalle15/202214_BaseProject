cb72254bba031350f9abe34d8e2af661fd02200a
Marisela Andrea Del Valle Morillo <marisela.valle@mercadolibre.com.co>
2022-09-17 11:52:44 -0500

    creado modulo, entidad y relaciones supermercado

---
+++ src/supermercado/supermercado.entity.ts
@@ -0,0 +1,24 @@
+import { CiudadEntity } from "src/ciudad/ciudad.entity";
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class SupermercadoEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    longitud: number;
+
+    @Column()
+    latitud: number;
+
+    @Column()
+    pag_web: string;
+
+    @ManyToMany(() => CiudadEntity, (ciudad) => ciudad.supermercados)
+    @JoinTable()
+    ciudades: CiudadEntity[];
+}
